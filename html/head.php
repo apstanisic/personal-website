de4f5437f1e163a291fe918ed01a906f5d46c5a7
Aleksandar <apstanisic@gmail.com>
2016-11-02 01:09:41 +0100

    Init 1.2

---
+++ html/head.php
@@ -0,0 +1,10 @@
+<title>AlStn</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Web prezentacija Aleksandra Stanisica, Web Developera, i njegovo vezbanje">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400,700,900&amp;subset=latin-ext" rel="stylesheet">
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		include('html/head.php');
+	?>
+</head>
+<body>
+	<?php
+		include('html/body.php');
+	?>
+
+	<!-- JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
