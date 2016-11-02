a7f816d26fd25469603a914fb7d38e0ff6cfa895
Aleksandar <apstanisic@gmail.com>
2016-11-02 15:16:55 +0100

    Zamenjen font, ne svidja mi se izgled linkova i teksta

--- html/head.php
+++ html/head.php
@@ -4,7 +4,7 @@
 <meta name="description" content="Web prezentacija Aleksandra Stanisica, Web Developera, i njegovo vezbanje">
 <link rel="shortcut icon" type="image/png" href="slike/favicon.png">
 <!-- Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Trocchi|Work+Sans:400,700,900&amp;subset=latin-ext" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700&amp;subset=cyrillic,latin-ext" rel="stylesheet">
 
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="css/style.css">
