e7a31331ffe88a3bc2183126a211d6023b2e4694
Aleksandar <apstanisic@gmail.com>
2016-11-04 22:44:22 +0100

    Gotov contact

--- html/include/contact.php
+++ html/include/contact.php
@@ -5,15 +5,15 @@
 		<h3>Posaljite poruku</h3>
 		<div>
 			<label for="contact-ime">Ime</label>
-			<input type="text" id="contact-ime" name="ime" placeholder="Stefan" autocomplete="on" required>
+			<input type="text" id="contact-ime" name="ime" placeholder="Nikola" autocomplete="on" required>
 		</div>
 		<div>
 			<label for="contact-email">E-mail</label>
-			<input type="email" id="contact-email"  name="mejl" placeholder="stefan@example.com" autocomplete="on" required>
+			<input type="email" id="contact-email"  name="mejl" placeholder="nikola@example.com" autocomplete="on" required>
 		</div>
 		<div>
 			<label for="contact-sadrzaj" >Sadrzaj</label>
-			<textarea id="contact-sadrzaj" placeholder="Sadrzaj..." required></textarea>
+			<textarea id="contact-sadrzaj" placeholder="Sadrzaj poruke..." required></textarea>
 		</div>
 		<div>
 			<button type="submit" name="contact-submit" >Posalji</button>
