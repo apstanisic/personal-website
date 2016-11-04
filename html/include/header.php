5f7c37f12c58b786c8a75a2d46f1a052806bbfe7
Aleksandar <apstanisic@gmail.com>
2016-11-04 07:57:07 +0100

    Pokusavam da unapredim dom

--- html/include/header.php
+++ html/include/header.php
@@ -1,17 +1,14 @@
 <header class="navigacija">
-	
-	<nav>
-
-		<div class="otvarac">
-			<div class="logo"><h1>Aleksandar Stanisic</h1></div>
-			<div class="nav-dugme">
-				<span class="crta crta-gore"></span>
-				<span class="crta-sredina"></span>
-				<span class="crta crta-dole"></span>
-			<!-- ≡ -->
-			</div>
+	<div class="otvarac">
+		<div class="logo"><h1>Aleksandar Stanisic</h1></div>
+		<div class="nav-dugme">
+			<span class="crta crta-gore"></span>
+			<span class="crta-sredina"></span>
+			<span class="crta crta-dole"></span>
+		<!-- ≡ -->
 		</div>
-
+	</div>
+	<nav>
 		<ul class="nav-linkovi">
 			<!-- <li class="nav-strela"><a href="#">➔</a></li> -->
 			<li><a href="#cover">Pocetak</a></li>
