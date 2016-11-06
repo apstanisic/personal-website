ee78651ac07fc58a83b6564f71caeb15e6a6d5b6
Aleksandar <apstanisic@gmail.com>
2016-11-06 22:08:19 +0100

    Dodat favicon, 'opet?'

--- html/head.php
+++ html/head.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Web prezentacija Aleksandra Stanisica, Web Developera, i njegovo vezbanje">
-<link rel="shortcut icon" type="image/ico" href="slike/neTreba/favicon.ico">
+<link rel="shortcut icon" type="image/ico" href="slike/favicon.ico">
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700&amp;subset=cyrillic,latin-ext" rel="stylesheet">
 
