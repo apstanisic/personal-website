c0377a7a8266302ff7719871689b530d9e1ca7f5
Aleksandar <apstanisic@gmail.com>
2016-11-12 07:41:43 +0100

    Funkcija odradjena u JavaScriptu, a ne u JQuery-ju

--- js/script.js
+++ js/script.js
@@ -1,10 +1,57 @@
-$(document).ready(function(){
-	$('.nav-dugme').click(function(){
-		$('.nav-linkovi').toggleClass('prikazi-nav');
-		$(this).toggleClass('pomeri-nav-dugme');
-		//$('.crta-sredina').toggleClass('rotiraj-dugme');
 
-	});
+// Vars
+var navLinkovi = document.querySelector('.nav-linkovi');
+var dugme = document.querySelector('.nav-dugme');
 
 
-});
+//Funkcije
+function prikaziNavigaciju(event){
+	navLinkovi.classList.toggle('prikazi-nav');
+	this.classList.toggle('pomeri-nav-dugme');
+
+}
+
+//Dogadjaji
+dugme.addEventListener('click', prikaziNavigaciju);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// 	$('.nav-dugme').click(function(){
+// 		$('.nav-linkovi').toggleClass('prikazi-nav');
+// 		$(this).toggleClass('pomeri-nav-dugme');
+// 		//$('.crta-sredina').toggleClass('rotiraj-dugme');
+
+// 	});
+
+
+// });
