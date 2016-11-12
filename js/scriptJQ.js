de0aab8539a7b00d2a3f676aba51238de1fcfd6f
Aleksandar <apstanisic@gmail.com>
2016-11-12 07:44:29 +0100

    Dodat fajl gde cu cuvati jquery kod, iako ga verovatno ne koristim

---
+++ js/scriptJQ.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.nav-dugme').click(function(){
+		$('.nav-linkovi').toggleClass('prikazi-nav');
+		$(this).toggleClass('pomeri-nav-dugme');
+		//$('.crta-sredina').toggleClass('rotiraj-dugme');
+
+	});
+
+
+});
