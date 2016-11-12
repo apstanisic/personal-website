5fa0e93420c5a4504aecb7f777475a43c2cb84c5
Aleksandar <apstanisic@gmail.com>
2016-11-12 09:42:56 +0100

    Uradjen ceo regex i sva provera

--- html/include/contact.php
+++ html/include/contact.php
@@ -1,22 +1,22 @@
 <section class="contact" id="contact">
 
 
-	<form method="post" class="contact-form">
+	<form id="contact-form" name="contact-form" method="post" class="contact-form">
 		<h3>Posaljite poruku</h3>
 		<div>
 			<label for="contact-ime">Ime</label>
-			<input type="text" id="contact-ime" name="ime" placeholder="Nikola" autocomplete="on" required>
+			<input type="text" id="contact-ime" name="name" placeholder="Nikola" autocomplete="on" required>
 		</div>
 		<div>
 			<label for="contact-email">E-mail</label>
-			<input type="email" id="contact-email"  name="mejl" placeholder="nikola@example.com" autocomplete="on" required>
+			<input type="email" id="contact-email"  name="email" placeholder="nikola@example.com" autocomplete="on" required>
 		</div>
 		<div>
 			<label for="contact-sadrzaj" >Sadrzaj</label>
-			<textarea id="contact-sadrzaj" placeholder="Sadrzaj poruke..." required></textarea>
+			<textarea id="contact-sadrzaj" placeholder="Sadrzaj poruke..." name="sadrzaj" required></textarea>
 		</div>
 		<div>
-			<button type="submit" name="contact-submit" >Posalji</button>
+			<button type="submit" id="dugme-submit" name="contact-submit" >Posalji</button>
 		</div>
 
 	</form>
--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 // Vars
 var navLinkovi = document.querySelector('.nav-linkovi');
 var dugme = document.querySelector('.nav-dugme');
+var contactForm = document.querySelectorAll('#contact-form input, #contact-form textarea');
+var dugmeForma = document.querySelector('#dugme-submit');
 
+
+
+//RegEx
+var emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+var nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+var sadrzajRegex = /^.{5,}$/;
 
 //Funkcije
 function prikaziNavigaciju(event){
@@ -11,7 +19,77 @@ function prikaziNavigaciju(event){
 
 }
 
+function proveriFormu(event){
+	
+		let provera = this.getAttribute("name");
+		let punoIme = eval(provera +"Regex");
+		let j = 0;
+
+		if(event.type !=="blur"){
+			if(punoIme.test(this.value)){
+				this.style.boxShadow = '0px 0px 10px 2px rgba(124, 187, 0, 0.8)';
+			} else {
+				this.style.boxShadow = '0px 0px 10px 2px rgba(255, 255, 255, 0.5)';
+			}
+		}
+		else
+		{
+			if(!punoIme.test(this.value)){
+				this.style.boxShadow = '0px 0px 10px 2px rgba(234, 67, 53, 0.5)';
+			}
+		}
+
+		// Ispituje da li su svi islovi ispunjeni, ako jesu daje animaciju dugmetu
+		for(var i = 0; i < contactForm.length; i++){
+			let proveraSve = contactForm[i].getAttribute("name");
+			let punoImeSve = eval(proveraSve +"Regex");
+
+			if(!punoImeSve.test(contactForm[i].value)){
+				j++;
+			}
+		}
+
+		if(j===0){
+			dugmeForma.style.animation= 'dugme-an 3000ms infinite';
+		}else{
+			dugmeForma.style.animation= 'none';
+		}try{
+	}
+	catch(e){
+		// Bacice gresku ako nisu data dobra imena u html-u ili js-u
+		//regex treba da se zove htmlIme + Regex
+	}
+}
+
+
+
 //Dogadjaji
 dugme.addEventListener('click', prikaziNavigaciju);
 
+for(var i = 0; i < contactForm.length; i++){
+	//contactForm[i].addEventListener('keyup', proveriFormu);
+	contactForm[i].addEventListener('input', proveriFormu);
+	contactForm[i].addEventListener('focus', proveriFormu);
+	contactForm[i].addEventListener('blur', proveriFormu);
+	//contactForm[i].addEventListener('blur', proveriSve);
 
+
+}
+
+
+
+
+
+
+
+
+
+	//window[provera+"Regex"]
+
+
+// 	function animirajPosalji(){
+// 	if(true){
+// 		dugme.style.animation= 'dugme-an 3000ms infinite';
+// 	}
+
+// }
