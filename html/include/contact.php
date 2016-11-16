740fe1d30167252811c4127175dfdd97e1f22c35
Aleksandar <apstanisic@gmail.com>
2016-11-16 09:35:39 +0100

    Napravljen bolji grid za contact preko 950px

--- html/include/contact.php
+++ html/include/contact.php
@@ -3,11 +3,11 @@
 
 	<form id="contact-form" name="contact-form" method="post" class="contact-form">
 		<h3>Posaljite poruku</h3>
-		<div>
+		<div class="skupi">
 			<label for="contact-ime">Ime</label>
 			<input type="text" id="contact-ime" name="name" placeholder="Nikola" autocomplete="on" required>
 		</div>
-		<div>
+		<div class="skupi">
 			<label for="contact-email">E-mail</label>
 			<input type="email" id="contact-email"  name="email" placeholder="nikola@example.com" autocomplete="on" required>
 		</div>
