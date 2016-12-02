f84d35eb1349fbe7441de08763fd95146a217f4f
Aleksandar <apstanisic@gmail.com>
2016-12-02 13:31:37 +0100

    Mala poboljsanja

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ var kontaktForma = (function(){
     var dugme = document.querySelector('#dugme-submit');
     //Funkcije
     // Proverava polje iz forme, sa regexom istog imena iz var rerex-a, vraca true, ako ispunjava uslov
-    function _proveriPolje(polje){
+    function proveriIzraz(polje){
         let naziv = polje.getAttribute("name");
         let uslov = regex[naziv];
         let valja = uslov.test(polje.value);
@@ -30,31 +30,12 @@ var kontaktForma = (function(){
     }
     /// FUNKCIJA init NE RADI!!!!!!!!!!!!!!!!!!!!!!!!!! JOS UVEK FIXME TODO FIXME
     function init(event){
-        obojiPolje(event);
-        proveriFormu(event);
+        obojiPolje(event); // TODO TODO
+        proveriFormu(event); //TODO TODO
         ///Napraviti da ovo radi, nece da prosledjuje this koji mi treba, valjda prosledjuje event?????
     }
-    // Proverava svako tekstualno polje u formi, dodaje animaciju dugmetu ako radi
-    function proveriFormu(event){
-        for (var i = 0; i < forma.length; i++) {
-            let valja = _proveriPolje(forma[i]);
-            if (!valja){
-                dugme.style.animation = 'none';
-                dugme.style.background = '#fff';
-                dugme.style.color = '#333334';
-                return;
-                //Prekida funkciju cim regex ne valja
-            }
-        }
-        // boji dugme u zeleno i dodaje animaciju
-        dugme.style.animation = 'dugme-an 2000ms infinite';
-        dugme.style.background = 'rgba(124, 187, 0, 1)';
-        dugme.style.color = '#fff';
-    }
-
     // Proverava samo polje na kom je doslo do eventa.
     // Kod bi bio lepsi kada bih mogao da pozovem ovu funkciju gore, ali ne znam lepo kad se sta prosledjuje TODO
-
     function obojiPolje(event) {
         try {
             let senka = {
@@ -62,9 +43,8 @@ var kontaktForma = (function(){
                 bela : '0px 0px 10px 2px rgba(255, 255, 255, 0.5)',
                 crvena : '0px 0px 10px 2px rgba(234, 67, 53, 0.5)'
             }
-            let valja = _proveriPolje(this);
+            let valja = proveriIzraz(this);
             //Zasto ne moze var senka = this.style.boxShadow; ????????????? BUG
-
             // pozadina textboxa pozeleni kada se iskoristi autocomple ??? BUG ??? FIXME???
             if (event.type !== "blur") {
                 if (valja) {
@@ -84,8 +64,29 @@ var kontaktForma = (function(){
         }
     }
 
+    // Proverava svako tekstualno polje u formi, dodaje animaciju dugmetu ako radi
+    function proveriFormu(){
+        for (var i = 0; i < forma.length; i++) {
+            let valja = proveriIzraz(forma[i]);
+            if (!valja){
+                dugme.style.animation = 'none';
+                dugme.style.background = '#fff';
+                dugme.style.color = '#333334';
+                return false;
+                //Prekida funkciju cim regex ne valja
+            }
+        }
+        // boji dugme u zeleno i dodaje animaciju
+        dugme.style.animation = 'dugme-an 2000ms infinite';
+        dugme.style.background = 'rgba(124, 187, 0, 1)';
+        dugme.style.color = '#fff';
+    }
 //Ovo je najruznije i najvaznije. Proslediti prave parametre u funkciju, tako da this radi...
 // TODO TODO TODO TODO TODO TODO
+/// FIXME FIXME FIXME FIXME
+///////////////////////////////////////////////////////////////////////////////////////
+
+//.bind(this) ovako nekako da prosledim this????? TODO
     for (var i = 0; i < forma.length; i++) {
         forma[i].addEventListener('input', obojiPolje);
         forma[i].addEventListener('focus', obojiPolje);
@@ -93,5 +94,7 @@ var kontaktForma = (function(){
         forma[i].addEventListener('input', proveriFormu);
         forma[i].addEventListener('focus', proveriFormu);
         forma[i].addEventListener('blur', proveriFormu);
+
+        //forma[i].addEventListener('blur', init);
     }
 })();
