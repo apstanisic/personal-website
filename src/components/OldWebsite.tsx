c29faf191ca9c9f37aa50d8c4c9dc45f3c30a7d8
Aleksandar <apstanisic@gmail.com>
2019-06-16 16:53:17 +0200

    Add old website link

--- src/components/OldWebsite.tsx
+++ src/components/OldWebsite.tsx
@@ -4,6 +4,10 @@ import { T } from '../core/i18n';
 import Section from './ui/Section';
 
 export default function OldWebsite() {
+  function goToOldWebsite() {
+    window.location.href = 'https://stanisic-old.netlify.com';
+  }
+
   return (
     <Section className="text-center md:px-3 flex flex-col" id="old-website">
       <T.span
@@ -11,11 +15,7 @@ export default function OldWebsite() {
         className="w-full lg:w-2/3 lg:mx-auto py-5 text-xl md:text-2xl"
       />
 
-      <Button
-        onClick={() => window.history.pushState({}, '', 'old-website')}
-        color="bg-blue-600"
-        className="text-xl"
-      >
+      <Button onClick={goToOldWebsite} color="bg-blue-600" className="text-xl">
         {T.translate('oldWebsite.button')}
       </Button>
     </Section>
