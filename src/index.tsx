68dd512dfb8341aee74e180a5054c4ebc5de7965
Aleksandar <apstanisic@gmail.com>
2019-06-19 08:02:42 +0200

    Try to implement tailwind normaly

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import 'normalize.css';
 import App from './components/App';
 // import 'normalize.css';
 import './style/index.css';
-import './style/tailwind.css';
 import { Providers } from './core/state';
 
 export default function Main() {
