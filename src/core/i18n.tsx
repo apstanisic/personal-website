891dce2e644089d76c57cf34fd5045d73fbabfff
Aleksandar Stanisic <aleksandar@tuta.io>
2020-07-08 15:49:42 +0200

    Fix merge 2

--- src/core/i18n.tsx
+++ src/core/i18n.tsx
@@ -1,57 +1,61 @@
-import { h } from "preact";
-import { useState } from "preact/hooks";
-import { storage } from "./storage";
-import { MDText } from "i18n-react";
+import { createContext, h } from "preact";
+import { useContext, useState } from "preact/hooks";
+// import { MDText } from "i18n-react";
 import en from "../assets/lang/en.json";
 import sr from "../assets/lang/sr.json";
-import { createContext } from "preact";
+import { storage } from "./storage";
 
 /** Available languages */
 type Language = "sr" | "en";
 // Objects that contain all language strings
 const languageObjects = { en, sr };
 
-// Renders text
-/**
- * Renders text
- * @Todo There is currently some Preact/Typescript error. Remove as any from T,
- * and add declaration.d.ts file from preact template
- */
-export const T = new MDText(en) as any;
-
 interface LangState {
   // Current Language
   language: Language;
-  // Toggle between Serbian and English
-  toggleLanguage: () => Promise<void>;
+  // Translator for given key
+  t: (key: string) => string;
+  // Toggle language between Serbian and English
+  toggleLanguage: () => void;
 }
 
 export const LangContext = createContext<LangState>(undefined as any);
 
 /** Use language state */
 export function Lang(props: { children: any }) {
-  const [language, setLanguage] = useState<Language>("en");
-
-  /** Change app language */
-  async function changeLanguage(lang: Language): Promise<void> {
-    await storage.set("lang", lang);
-    T.setTexts(languageObjects[lang]);
-    setLanguage(lang);
-  }
+  const dbLang = storage.get("lang");
+  const [language, setLanguage] = useState<Language>(dbLang ?? "en");
+  const [langData, setLangData] = useState<Record<string, any>>(languageObjects[language]);
 
   /** Toggle app language */
-  async function toggleLanguage(): Promise<void> {
-    changeLanguage(language === "en" ? "sr" : "en");
+  function toggleLanguage(): void {
+    const newLang = language === "en" ? "sr" : "en";
+    setLanguage(newLang);
+    setLangData(newLang === "en" ? en : sr);
+    storage.set("lang", newLang);
   }
 
-  // Sets language from database
-  storage.get("lang").then(lang => {
-    changeLanguage(lang === undefined ? "en" : (lang as Language));
-  });
+  /**
+   * Simple helper function for geting value from nested object
+   * Source:
+   * https://stackoverflow.com/questions/6393943/convert-javascript-string-in-dot-notation-into-an-object-reference
+   * @param key to requested value
+   */
+  function t(key: string): string {
+    return (key.split(".").reduce((o, i) => o?.[i], langData) as unknown) as string;
+  }
 
   return (
-    <LangContext.Provider value={{ language, toggleLanguage }}>
+    <LangContext.Provider value={{ language, toggleLanguage, t }}>
       {props.children}
     </LangContext.Provider>
   );
+}
+
+/**
+ * Helper hook for easier transaltion use
+ */
+export function useT() {
+  const { t } = useContext(LangContext);
+  return t;
 }
